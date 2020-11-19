0f2c0f3908131bf7a4ce0e5a94d1074bdc4504e6
pablo.berrotaran <pberrotaran@keepcon.com>
2020-11-19 02:44:28 -0300

    Cleanup

--- server_src/Protocol.cpp
+++ server_src/Protocol.cpp
@@ -22,34 +22,4 @@ void HTTPProtocol::handleSocket(Socket& socket) {
 
 	std::unique_ptr<HTTPRequest> request = request_factory(stream.str());
 	std::unique_ptr<HTTPResponse> response = request->execute(this->resources);
-	/*
-	std::string word;
-	std::string resource_name;
-	stream >> word;
-	stream >> resource_name;
-
-	std::cout << word + " " + resource_name + " HTTP/1.1" << std::endl;
-
-	std::stringstream stream;
-	char buffer[BUFFER_SIZE] = {0};
-	while (socket.receive(buffer, BUFFER_SIZE-1) != 0) stream << buffer;
-
-
-	std::string resource_name;
-	stream >> resource_name;
-
-	if (word.compare("POST") == 0) {
-		std::string message = stream.str();
-
-		size_t begin = message.find("\n\n");
-		size_t end = message.npos;
-
-		std::string body = message.substr(begin + 2, end);
-
-	} else if (word.compare("GET") == 0) {
-		// request de get
-	} else {
-		// manejar el pedido invalido
-	}
-	*/
 }
