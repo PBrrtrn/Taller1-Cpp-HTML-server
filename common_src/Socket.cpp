29f614c709bae7ffc7c96dda6dfafe480516e795
pablo.berrotaran <pberrotaran@keepcon.com>
2020-11-20 00:38:21 -0300

    Socket cleanup

--- common_src/Socket.cpp
+++ common_src/Socket.cpp
@@ -109,19 +109,6 @@ int Socket::send(const char *data, size_t data_size) {
 }
 
 int Socket::receive(char *buffer, size_t n_bytes) {
-	/* 
-	Lo que está pasando es que en receive, el servidor tiene
-		> total_bytes_received = 0, bytes_received = 0
-		> hace el primer recv
-		> total_bytes_received = 6, bytes_received = 6
-		Pero la condición del while es que no haya recibido n_bytes (64 bytes)
-
-		Por lo tanto, vuelve a loopear y espera un receive.
-			-> Antes, como el cliente se cerraba, el segundo recv era inmediatamente cero así que se
-				 hacía break del while.
-
-			Ahora, como el cliente sigue abierto, no llega el segundo recv, y el servidor se queda
-	*/
   size_t total_bytes_received = 0;
   int bytes_received = 0;
   while (total_bytes_received < n_bytes) {
@@ -142,11 +129,11 @@ void Socket::shutdown() {
 }
 
 void Socket::shutdown_write() {
-	if (this->fd != -1) ::shutdown(this->fd, SHUT_WR);
+	::shutdown(this->fd, SHUT_WR);
 }
 
 void Socket::close() {
-	if (this->fd != -1)	::close(this->fd);
+	::close(this->fd);
 }
 
 struct addrinfo* Socket::initializeAddrinfo(const char *host,
